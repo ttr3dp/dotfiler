ff84533d392001b33552f2d21548e50bcea45044
Aleksandar Radunovic <aleksandar@radunovic.io>
2018-05-27 11:32:30 +0200

    Rename file_utils -> utils

--- lib/dotfiler/file_system.rb
+++ lib/dotfiler/file_system.rb
@@ -1,28 +1,27 @@
-require "pathname"
 require "fileutils"
 
 module Dotfiler
   class FileSystem
-    attr_reader :file_utils
+    attr_reader :utils
 
-    def initialize(file_utils = FileUtils)
-      @file_utils = FileUtils
+    def initialize(utils: FileUtils)
+      @utils = utils
     end
 
     def move(source_path, destination_path, *options)
-      file_utils.move(source_path, destination_path, *options)
+      utils.move(source_path, destination_path, *options)
     end
 
     def copy(source_path, destination_path, *options)
-      file_utils.cp_r(source_path, destination_path, *options)
+      utils.cp_r(source_path, destination_path, *options)
     end
 
     def symlink(source, destination, *options)
-      file_utils.symlink(source, destination, *options)
+      utils.symlink(source, destination, *options)
     end
 
     def remove(path)
-      file_utils.remove_entry_secure(path)
+      utils.remove_entry_secure(path)
     end
 
     def create_file(name, content = "")
@@ -32,13 +31,13 @@ module Dotfiler
     end
 
     def create_dir(path, *options)
-      file_utils.mkdir_p(path, *options)
+      utils.mkdir_p(path, *options)
     end
 
     def execute(command, *command_options, **options)
       full_command = ([command] + command_options).join(" ")
 
-      options[:capture] == true ?  `#{full_command}` : system(command, *options)
+      options[:capture] == true ?  `#{full_command}` : system(full_command)
     end
   end
 end
