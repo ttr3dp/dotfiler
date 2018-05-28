41a4b6d66bbff997876d96dd273c66ba0e846478
Aleksandar Radunovic <aleksandar@radunovic.io>
2018-05-28 16:32:18 +0200

    Minor refactoring

--- lib/dotfiler/dotfile.rb
+++ lib/dotfiler/dotfile.rb
@@ -2,20 +2,17 @@ module Dotfiler
   class Dotfile
     attr_reader :name, :link, :path
 
-    def initialize(args)
-      @name = args.fetch(:name)
-      @link = to_path.(args.fetch(:link))
-      @path = to_path.(args.fetch(:path))
+    def initialize(name:, link:, path:)
+      @name = name
+
+      to_path = Dotfiler.resolve["to_path"]
+
+      @link = to_path.(link)
+      @path = to_path.(path)
     end
 
     def to_h
       { name: name, link: link.to_s, path: path.to_s }
-    end
-
-    private
-
-    def to_path
-      Dotfiler.resolve["to_path"]
     end
   end
 end
